9ae1b6127f2e1bf3c003a13e74e2995b3a516c02
nsharper <nharper@unomaha.edu>
2019-03-21 18:54:09 -0500

    added __str__ values to each model

--- api/models.py
+++ api/models.py
@@ -21,8 +21,8 @@ class Organization(models.Model):
 	state = models.CharField(max_length=2, blank=False)
 	zipcode = models.CharField(max_length=20, blank=False)
 
-        def __str__(self):
-                return self.name
+	def __str__(self):
+		return self.name
 
 ADMIN = 'ADM'
 LENDER = 'LEN'
@@ -34,30 +34,34 @@ class UserProfile(models.Model):
 	user = models.OneToOneField(User, on_delete=models.CASCADE)
 	org = models.ForeignKey(Organization, blank=False)
 	roles = models.TextField(max_length=200, blank=True, choices=USER_ROLE_CHOICES)
-        
-        def __str__(self):
-                return self.user.username
-                        
+
+	def __str__(self):
+		return self.user.username
+
 
 
 class ItemType(models.Model):
 	name = models.CharField(max_length=200, blank=False)
 	description = models.TextField(max_length=1000, blank=True)
 	imagepath = models.TextField(max_length=200, blank=False, default="notfound.jpg")
-        
-        def __str__(self):
-                return self.name
+
+	def __str__(self):
+		return self.name
 
 class Cart(models.Model):
 	user = models.ForeignKey(UserProfile, blank=True)   # The idea here is that every logged in user will have
                                                      # a cart, but a user who is not logged in could also have
                                                      # one
-        
+	def __str__(self):
+		return "Cart: %s" % self.user.user
 
 class CartItemTypeRel(models.Model):
-        cart = models.ForeignKey(UserProfile, blank=False)
-        itemtype = models.ForeignKey(ItemType, blank=False)
+	cart = models.ForeignKey(UserProfile, blank=False)
+	itemtype = models.ForeignKey(ItemType, blank=False)
 	quantity = models.PositiveIntegerField(blank=False, default=1)
+
+	def __str__(self):
+		return "Cart: %s -> ItemType: %s" % (self.cart, self.itemtype)
 
 AVAIL = 'AVA'
 CO = 'CO'
@@ -79,8 +83,8 @@ class Item(models.Model):
 	owner = models.ForeignKey(UserProfile, blank=False)
 	checkedoutto = models.ForeignKey(UserProfile, null=True, related_name='items')
 
-        def __str__(self):
-                return "%s--%s" % (self.type.name, self.id)
+	def __str__(self):
+		return "Item: %s ID: %s" % (self.type.name, self.id)
 
 class History(models.Model):
 	user = models.ForeignKey(UserProfile, blank=False)
@@ -89,24 +93,28 @@ class History(models.Model):
 	returnedon = models.DateField(null=True, blank=True)
 	missingparts = models.TextField(max_length=1000, blank=True)
 
+	def __str__(self):
+		return "History ID:%s User: %s Returned: %s" % (self.id, self.user, self.returnedon)
+
 
 class HistoryItemRel(models.Model):
-        history = models.ForeignKey(History, blank=False)
-        item = models.ForeignKey(Item, related_name='items', blank=False)
+	history = models.ForeignKey(History, blank=False)
+	item = models.ForeignKey(Item, related_name='items', blank=False)
 
-
+	def __str__(self):
+		return "History: %s -> Item: %s" % (self.history, self.item)
 
 ORDERED = 'ORD'
 INFUL = 'INFUL'
 FUL = 'FUL'
 SHIPPED = 'SHIP'
-RECIEVED = 'REC'
+RECEIVED = 'REC'
 ORDER_STATUS_CHOICES = (
 	(ORDERED,'Ordered'),
 	(INFUL, 'In Fulfillment'),
 	(FUL, 'Fulfilled'),
 	(SHIPPED, 'Shipped'),
-	(RECIEVED, 'Return Recieved'),
+	(RECEIVED, 'Return Received'),
 )
 
 class Order(models.Model):
@@ -118,6 +126,9 @@ class Order(models.Model):
 	returnedon = models.DateField(null=True, blank=True)
 	missingparts = models.TextField(max_length=1000, blank=True)
 
+	def __str__(self):
+		return "Order ID:%s" % self.id
+
 # class OrderItemsRel(models.Model):
 #      order = models.ForeignKey(Order, on_delete=models.CASCADE, blank=False)
 #      item = models.ForeignKey(Item, related_names='items', blank=False)
@@ -125,23 +136,23 @@ class Order(models.Model):
 class Package(models.Model):
 	name = models.CharField(max_length=200, blank=False)
 	description = models.TextField(max_length=1000, blank=False)
-	items = models.ManyToManyField( 
+	items = models.ManyToManyField(
 		ItemType,
 		through='PackageItemTypeRel',
 		through_fields=('package', 'itemtype'),
 	)
 
-        def __str__(self):
-                return self.name
-	
-	
+	def __str__(self):
+		return self.name
+
+
 class PackageItemTypeRel(models.Model):
 	package = models.ForeignKey(Package, blank=False)
 	itemtype = models.ForeignKey(ItemType, blank=False)
 	quantity = models.PositiveIntegerField(blank=True, default=1)
 
-        def __str__(self):
-                return "%s->%s" % (self.package.name, self.itemtype.name)
+	def __str__(self):
+		return "Package: %s -> ItemType: %s" % (self.package.name, self.itemtype.name)
 
 
 class OrganizationSerializer(serializers.ModelSerializer):
@@ -179,4 +190,3 @@ class PackageItemTypeRelSerializer(serializers.ModelSerializer):
         class Meta:
                 model = PackageItemTypeRel
                 fields = ('id','package','itemtype', 'quantity')
-
